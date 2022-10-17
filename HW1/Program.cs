33458b510b760ad44f83d3701f2b3c902a2cc94f
Artem Kolomiiets <akolomiets02@gmail.com>
2022-10-17 21:45:01 +0300

    testing

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Artem";
+            string lastname = "Kolomiiets";
+            int age = 20; //можем и после этого подсчитать количество лет до новой жизни в параме, но ты сказал, что можно и так)
+            Console.WriteLine($"Name: {name} {lastname}, NEW LIFE COUNT: {40 - age} years");
+        }
+    }
+}
